10cf8d85b85db9a74c4c77289878a2d9a793732a
Tamding <tamding@Tamdings-MacBook-Air.local>
2019-11-08 12:42:32 +0530

    added code for validate csv

--- src/main/python/settings.py
+++ src/main/python/settings.py
@@ -1,3 +1,4 @@
 LOGIN_CREDENTIALS = { 'user123':'password123', 'userXYZ':'passwordXYZ'}
 
-COLUMNS_LIST = ['column1', 'column2', 'column3']
+#COLUMNS_LIST = ['column1', 'column2', 'column3']
+COLUMNS_LIST = ['organization', 'Industry', 'City/State', 'postedby', 'title', 'description', 'experience', 'Exp-Category', 'skill', 'salary', 'Approx_Date']
