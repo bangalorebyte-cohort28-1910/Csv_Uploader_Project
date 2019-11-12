bea2110a64074b97113a32e6d6ca518f04f343df
suvarna.purohit@gmail.com <suvarna_purohit@yahoo.com>
2019-11-12 16:11:46 +0530

    Updated changes to handle the NoneType condition, Updated unittest file for send_csv()

--- test/main/python/test_controller.py
+++ test/main/python/test_controller.py
@@ -1,32 +1,43 @@
 import sys, os, unittest
 sys.path.insert(1, os.path.dirname('controller.py'))
-import src.main.python.controller as controller
+from src.main.python.controller import Controller
 import pandas as pd
  
-class TestController(unittest.TestCase): 
+class TestController(unittest.TestCase):
  
-    # def setUp(self):
-    #     self.df = pd.read_csv("Sample_valid.csv")
+    def setUp(self):
+        self.df = pd.read_csv("Sample_valid.csv")
+        self.df1 = pd.read_csv("Sample_invalid1.csv")
+        self.controllerobj = Controller()
 
 
     def test_validateLogin(self):
-        self.assertEqual(controller.validate_login('username123', 'password1234'), "INVAID_USERNAME")
-        self.assertEqual(controller.validate_login('user123', 'password1234'), "INVALID_PASSWORD")
-        self.assertEqual(controller.validate_login('user123', 'password123'), "VALID_USER")
+        self.assertEqual(self.controllerobj.validate_login('username123', 'password1234'), "INVAID_USERNAME")
+        self.assertEqual(self.controllerobj.validate_login('user123', 'password1234'), "INVALID_PASSWORD")
+        self.assertEqual(self.controllerobj.validate_login('user123', 'password123'), "VALID_USER")
+       
 
- 
     def test_validateCSV(self):
         #self.assertEqual( controller.validate_csv(), True)
-        self.assertEqual( controller.validate_csv("Sample_invalid1.xlsx"), "INVALID_CSV")
-        self.assertEqual(controller.validate_csv("Sample_invalid2.xlsx"), "INVALID_CSV")
-        self.assertEqual(controller.validate_csv("Sample_valid.xlsx"), "VALID_CSV")
-        self.assertEqual(controller.validate_csv("Sample_valid.csv"), "VALID_CSV")
-        self.assertEqual(controller.validate_csv("Sample_invalid1.csv"), "INVALID_CSV")
+        self.assertEqual(self.controllerobj.validate_csv("Sample_invalid1.xlsx"), "INVALID_CSV")
+        self.assertEqual(self.controllerobj.validate_csv("Sample_invalid2.xlsx"), "INVALID_CSV")
+        self.assertEqual(self.controllerobj.validate_csv("Sample_valid.xlsx"), "VALID_CSV")
+        self.assertEqual(self.controllerobj.validate_csv("Sample_valid.csv"), "VALID_CSV")
+        self.assertEqual(self.controllerobj.validate_csv("Sample_invalid1.csv"), "INVALID_CSV")
 
 
-    # def test_send_csv(self, self.df):
-    #     self.assertEqual(controller.send_csv(self.df), "DB_UPDATE_SUCCESSFUL")
-    #     self.assertEqual(controller.send_csv(self.df), "DB_UPDATE_FAILED")
+    def test_send_csv(self):
+        self.assertEqual(self.controllerobj.validate_csv("Sample_valid.csv"), "VALID_CSV")
+        self.assertEqual(self.controllerobj.send_csv(), "DB_UPDATE_SUCCESSFUL")
+        self.assertEqual(self.controllerobj.validate_csv("Sample_valid.xlsx"), "VALID_CSV")
+        self.assertEqual(self.controllerobj.send_csv(), "DB_UPDATE_SUCCESSFUL")
+        self.assertEqual(self.controllerobj.validate_csv("Sample_invalid2.xlsx"), "INVALID_CSV")
+        self.assertEqual(self.controllerobj.send_csv(), "DB_UPDATE_FAILED")
+        self.assertEqual(self.controllerobj.validate_csv("Sample_invalid1.xlsx"), "INVALID_CSV")
+        self.assertEqual(self.controllerobj.send_csv(), "DB_UPDATE_FAILED")
+        self.assertEqual(self.controllerobj.validate_csv("Sample_invalid1.csv"), "INVALID_CSV")
+        self.assertEqual(self.controllerobj.send_csv(), "DB_UPDATE_FAILED")
+
  
 if __name__ == '__main__':
     unittest.main()
